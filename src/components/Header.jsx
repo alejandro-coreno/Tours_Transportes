3ee814f74dd1c8fe1df3b1e6ddce44e4c005a4d4
josue <alejandro@MacBook-Air-de-josue.local>
2024-08-05 21:00:37 -0600

    Se agrega componente Header.astro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,40 @@
-import iconMenu from "../img/icons/icon_menu.png"
+import { useState } from 'react';
+import iconMenu from '../img/icons/icon_menu.png';
+import iconCerrar from "../img/icons/icon_cerrar.png"
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    const handlerClick = () => {
-        alert('Click en el menu');
-    }
+    const handleClick = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
-        <header className="bg-slate-500 flex justify-between p-5">
-            <div className="w-32 border border-red-300">
-                <img src="" alt="" />
+        <header className="bg-slate-400 border border-blue-600 p-4 sm:flex justify-between">
+            <div className="flex justify-between items-center">
+                <div className="w-28 h-10 border border-red-300">
+                    <img src="" alt="Logo" />
+                </div>
+                <img 
+                    className="w-10 h-10 cursor-pointer sm:hidden" 
+                    src={menuOpen ? iconCerrar.src : iconMenu.src } 
+                    alt="menu_icon" 
+                    onClick={handleClick} 
+                />
             </div>
 
-            <nav className="border border-red-300 py-2 px-1 hidden sm:block">
-                <ul className="flex gap-4">
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Unidades</a></li>
-                    <li><a href="#">Viajes</a></li>
-                    <li><a href="#">cotizador</a></li>
-                    <li><a href="#">Contacto</a></li>
-
-                 </ul>
+            <nav className={`border border-lime-300 py-3 px-2 sm:flex ${menuOpen ? 'block' : 'hidden'}`}>
+                <ul className="flex flex-col gap-6 sm:flex-row sm:gap-4">
+                    <li className='text-yellow-100 font-medium tracking-wide text-xl'><a href="#">Servicios</a></li>
+                    <li className='text-yellow-100 font-medium tracking-wide text-xl'><a href="#">Unidades</a></li>
+                    <li className='text-yellow-100 font-medium tracking-wide text-xl'><a href="#">Viajes</a></li>
+                    <li className='text-yellow-100 font-medium tracking-wide text-xl'><a href="#">Cotizador</a></li>
+                    <li className='text-yellow-100 font-medium tracking-wide text-xl'><a href="#">Contacto</a></li>
+                </ul>
             </nav>
-
-            <img 
-                className="sm:hidden"
-                src={iconMenu.src} 
-                alt="" 
-                id="logo"
-                onClick={ handlerClick }
-            />
         </header>
-    )
-}
+    );
+};
 
 export default Header;
+
