7b45b2b77a16af822079cdb13116f27ea94196fd
josue <alejandro@MacBook-Air-de-josue.local>
2024-08-07 15:00:51 -0600

    Header.jsx completado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,44 @@
 import { useState } from 'react';
-import iconMenu from '../img/icons/icon_menu.png';
-import iconCerrar from "../img/icons/icon_cerrar.png";
-import logo from "../img/icons/logo.png";
+import iconMenu from '../img/icons/icon_menu_F.png';
+import iconCerrar from '../img/icons/icon_cancelarF.png';
+import logo from '../img/icons/logo.png';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const handleClick = () => {
-        setMenuOpen(!menuOpen)
+        setMenuOpen(!menuOpen);
     };
 
     return (
-        <header className="bg-slate-300 z-[-1] flex-col gap-3 justify-between flex sm:flex-row border border-blue-600 p-4">
-            <div className="flex justify-between items-center">
-                <div className="w-20">
-                    <img src={logo.src} alt="Logo" className='w-20 bg-contain rounded-md'/>
+        <header className="bg-[#25324e] shadow sm:flex justify-between py-5">
+            <div className="flex justify-between px-3">
+                <div className='cursor-pointer'>
+                    <img src={logo.src} alt="" className='h-14 rounded-md' />
                 </div>
-                <img 
-                    className="w-10 cursor-pointer sm:hidden" 
-                    src={ menuOpen ? iconCerrar.src : iconMenu.src } 
-                    alt="Menu icon" 
-                    onClick={handleClick} 
-                />
+
+                <div className='p-2 cursor-pointer sm:hidden'>
+                    <img
+                        className='h-8'
+                        onClick={ handleClick }
+                        src={menuOpen ? iconCerrar.src: iconMenu.src} 
+                        alt=""
+                    />
+                </div>
             </div>
 
-            <nav className={`border border-lime-300 py-3 px-2 sm:block ${menuOpen ? ' block' : ' hidden'}`}>
-                <ul className="flex flex-col gap-5 sm:flex-row sm:gap-4">
-                    <li><a href="#servicios">Servicios</a></li>
-                    <li><a href="#unidades">Unidades</a></li>
-                    <li><a href="#">Viajes</a></li>
-                    <li><a href="#">Cotizador</a></li>
-                    <li><a href="#">Contacto</a></li>
+            <nav className={`bg-[#25324e] p-6 duration-500 ease-in absolute top-[-400px] sm:top-0 sm:w-auto sm:border left-0 
+                w-full sm:relative transition-all ${menuOpen && 'top-[80px] opacity-100'}`}>
+                <ul className='flex flex-col gap-4 sm:flex-row'>
+                    <li className='text-white font-light tracking-wide'><a href="#">Servicios</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#">Unidades</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#">Viajes</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#">Cotizador</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#">Contacto</a></li>
                 </ul>
-            </nav>
+            </nav>        
         </header>
+
     );
 };
 
