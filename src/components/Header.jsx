eaced2ef24585a7ca5fa7c819bd3dc268535f227
josue <alejandro@MacBook-Air-de-josue.local>
2024-08-06 21:29:54 -0600

    Prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import iconMenu from '../img/icons/icon_menu.png';
 import logo from "../img/icons/logo.png"
 import iconCerrar from '../img/icons/icon_cerrar.png';
+import "../styles/styles.css"
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -11,24 +12,24 @@ const Header = () => {
     };
 
     return (
-        <header className="w-full bg-slate-500 flex flex-col sm:flex-row justify-between py-5">
+        <header className="w-full h-20 bg-slate-500 flex flex-col  sm:flex-row justify-between shadow-md">
     
-        <div className="flex justify-between px-3">
-            <div className='w-20 border rounded-md'>
-                <img src={logo.src} alt="" className='w-full bg-cover bg-center  rounded-md' />
+        <div className="flex h-full items-center border border-blue-400 justify-between px-3">
+            <div className='w-20 border rounded-md '>
+                <img src={logo.src} alt="" className='w-20 bg-contain bg-center  rounded-md' />
             </div>
 
             <div className='w-20 border border-red-300 sm:hidden'>
                 <img
                     onClick={ handleClick }
-                    src={iconMenu ? iconCerrar.src: iconMenu.src} 
+                    src={menuOpen ? iconCerrar.src: iconMenu.src} 
                     alt=""
                 
                 />
             </div>
         </div>
 
-        <nav className={`bg-slate-500 p-4 duration-300 ease-linear absolute top-[-100%] sm:top-0 sm:w-auto sm:border left-0 w-full sm:relative ${menuOpen && 'top-[13%]'}`}>
+        <nav className={` bg-indigo-400 p-4 duration-300 ease-linear absolute top-[8.9%] h-custom sm:top-0 sm:w-auto sm:border right-[-100%] w-[80%] sm:relative sm:h-auto ${menuOpen && 'right-[-0]'}`}>
             <ul className='flex flex-col gap-4 sm:flex-row'>
                 <li><a href="#">Servicios</a></li>
                 <li><a href="#">Unidades</a></li>
@@ -38,9 +39,6 @@ const Header = () => {
 
             </ul>
         </nav>
-
-        
-
 </header>
     );
 };
