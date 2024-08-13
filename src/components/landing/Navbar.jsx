4fefaed33a24ae7b1fc73d1152030c59bb807281
josue <alejandro@MacBook-Air-de-josue.local>
2024-08-13 15:28:50 -0600

    Se agrega componente Unidades

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -10,6 +10,10 @@ const Navbar = () => {
         setMenuOpen(!menuOpen);
     };
 
+    const closeMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <nav className="bg-gradient-to-r from-[#0d161c] to-[#1e324d] shadow-2xl sm:flex justify-between py-5">
             <div className="flex justify-between px-3">
@@ -22,7 +26,7 @@ const Navbar = () => {
                         className='h-8'
                         onClick={ handleClick }
                         src={menuOpen ? iconCerrar.src: iconMenu.src} 
-                        alt=""
+                        alt="menu"
                     />
                 </div>
             </div>
@@ -30,11 +34,11 @@ const Navbar = () => {
             <div className={`bg-gradient-to-r from-[#0d161c] to-[#1e324d] p-6 duration-200 ease-in absolute top-[-400px] sm:top-0 sm:w-auto left-0 
                 w-full sm:static ${menuOpen && 'top-[80px] opacity-100'}`}>
                 <ul className='flex flex-col gap-5 sm:flex-row'>
-                    <li className='text-white font-light tracking-wide'><a href="#servicios">Servicios</a></li>
-                    <li className='text-white font-light tracking-wide'><a href="#">Unidades</a></li>
-                    <li className='text-white font-light tracking-wide'><a href="#">Viajes</a></li>
-                    <li className='text-white font-light tracking-wide'><a href="#">Cotizador</a></li>
-                    <li className='text-white font-light tracking-wide'><a href="#">Contacto</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#servicios" onClick={ closeMenu }>Servicios</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#unidades" onClick={ closeMenu}>Unidades</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#viajes" onClick={ closeMenu}>Viajes</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#cotizador" onClick={ closeMenu}>Cotizador</a></li>
+                    <li className='text-white font-light tracking-wide'><a href="#contacto" onClick={closeMenu}>Contacto</a></li>
                 </ul>
             </div>        
         </nav>
