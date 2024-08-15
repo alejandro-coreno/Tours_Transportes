d67c1749571c3cd6bdb7074c87c3233967f5c79d
josue <alejandro@MacBook-Air-de-josue.local>
2024-08-15 14:37:55 -0600

    Se agrega carusel v1

---
+++ src/data/unidades.js
@@ -0,0 +1,37 @@
+export const unidades = [
+    {
+        title: "Expeditión",
+        descripcion: "lorem ipuso loremm lomrelrem loremmm",
+        img: "public/unidades/transit.jpeg"
+    },
+
+    {
+        title: "Escape",
+        descripcion: "lorem lorem lorem iputo",
+        img: "public/unidades/transit.jpeg"
+    },
+
+    {
+        title: "Escape",
+        descripcion: "lorem lorem lorem iputo",
+        img: "public/unidades/transit.jpeg"
+    },
+
+    {
+        title: "Escape",
+        descripcion: "lorem lorem lorem iputo",
+        img: "public/unidades/transit.jpeg"
+    },
+
+    {
+        title: "Escape",
+        descripcion: "lorem lorem lorem iputo",
+        img: "public/unidades/transit.jpeg"
+    },
+    {
+        title: "Escape",
+        descripcion: "lorem lorem lorem iputo",
+        img: "public/unidades/transit.jpeg"
+    }
+
+]
